2e338cc2ea67dab829c101d4c2720589c46d6788
ArsamAryandoust <arsama@ethz.ch>
2023-03-04 00:19:44 +0100

    minor corrections climart

--- src/datasets/climart.py
+++ src/datasets/climart.py
@@ -46,7 +46,7 @@ def split_train_val_test(config_climart: dict):
     
     # corrections for testing
     list_of_years.remove('1995') # inputs
-    #list_of_years.remove('1851') # outputs pristine
+    list_of_years.remove('1851') # outputs pristine
     #list_of_years = [e for e in list_of_years if e not in approved_years]
     
     # create progress bar
