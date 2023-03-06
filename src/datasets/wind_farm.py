768fbad4a3fe169d0db0195b19cc65c34643654f
ArsamAryandoust <arsama@ethz.ch>
2023-03-06 19:38:14 +0100

    implementing wf

--- src/datasets/wind_farm.py
+++ src/datasets/wind_farm.py
@@ -20,8 +20,8 @@ def process_all_datasets(config: dict):
         df_data[['hour', 'minute']] = df_data.Tmstamp.str.split(':', expand=True)
         df_data.drop(columns=['Tmstamp'], inplace=True)
         # sort data
-        df_data.sort_values(by=['TurbID','Day', 'hour', 'minute'], inplace=True,
-            ignore_index=True)
+        #df_data.sort_values(by=['TurbID','Day', 'hour', 'minute'], inplace=True,
+        #    ignore_index=True)
         # expand data with sliding time window
         df_data = create_datapoints(config_wind, df_data)
         # split the loaded dataframe into training, validation and testing
@@ -33,7 +33,7 @@ def load_data(config_wind: dict) -> (pd.DataFrame, pd.DataFrame):
     Loads the SCADA data and the location data of each turbine from csv files,
     and returns these.
     """    
-    print('\nLoading data for Wind Farm task!')
+    print('\nLoading data for {}!'.format(config_wind['subtask']))
     df_locations = pd.read_csv(config_wind['path_to_turb_loc_file'])
     if config_wind['subtask'] == 'days_245':
         # all data is in single file
@@ -69,8 +69,15 @@ def create_datapoints(config_wind: dict, df_data: pd.DataFrame) -> pd.DataFrame:
     # get a list of all turbine IDs available in data
     turbine_list = list(set(df_data['TurbID']))
     turbine_list.sort()
-    
-    print(turbine_list)
+    # iterate over all turbine IDs
+    for turbine_id in turbine_list:
+        # get corresponding entries
+        df_turbine = df_data.loc[df_data['TurbID']==turbine_id]
+        # sort by time
+        df_turbine.sort_values(by=['Day', 'hour', 'minute'], inplace=True,
+            ignore_index=True)
+        
+        print(df_turbine['Day'])
     
     return df_data
     
