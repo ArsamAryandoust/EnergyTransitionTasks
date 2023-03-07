4eb871229c198309f4513ab7db84d19e12ce1ae9
ArsamAryandoust <arsama@ethz.ch>
2023-03-07 13:59:12 +0100

    minor change

--- src/datasets/wind_farm.py
+++ src/datasets/wind_farm.py
@@ -119,7 +119,7 @@ def create_datapoints(config_wind: dict, df_data: pd.DataFrame) -> pd.DataFrame:
         pbar.update(1)
     
     # create column name
-    col_name_list = ['TrbID', 'day', 'hour', 'minute']
+    col_name_list = ['TurbID', 'day', 'hour', 'minute']
     new_fseries_name_list = ['wind_speed', 'wind_direction', 'temperature_out',
     'temperature_in', 'nacelle_angle', 'blade1_angle', 'blade2_angle',
     'blade3_angle', 'reactive_power', 'active_power']
