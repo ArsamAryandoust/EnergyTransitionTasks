fc150976952509fc96ec4fe22336376cd15ce859
ArsamAryandoust <arsama@ethz.ch>
2023-04-06 14:39:55 +0200

    experimental commits starting from here

--- src/datasets/building_electricity.py
+++ src/datasets/building_electricity.py
@@ -181,7 +181,7 @@ def process_meteo_and_load_profiles(config_building: dict,
       meteo_dict = {}
       for meteo_name in config_building['meteo_name_list']:
         meteo_values = df_meteo[meteo_name].iloc[
-          range(i-config_building['historic_window'], i, 4)
+          list(range(i-config_building['historic_window'], i, 4))
           #(i-config_building['historic_window']):i
         ].values
         meteo_dict[meteo_name] = meteo_values
