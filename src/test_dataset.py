8245b54ccb56a043ced9dbcb3ed9bbbb1da95479
ArsamAryandoust <arsama@ethz.ch>
2023-04-08 20:58:02 +0200

    fixes to test

--- src/test_dataset.py
+++ src/test_dataset.py
@@ -3,11 +3,12 @@ import selberai.data.load_data as load_data
 def test(config: dict, dataset_name: str):
   """
   """
+  print('Testing {}'.format(dataset_name))
   
   # set the path to data we want to load
   path_to_data = config['general']['path_to_data']
   
   # load data
-  train, val, test = load_data(dataset_name, path_to_data=path_to_data)
+  train, val, test = load_data.load(dataset_name, path_to_data=path_to_data)
   
   print("Successfully tested {}!".format(dataset_name))
