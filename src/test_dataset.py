5c511d86ded3c53793881967edff18b5581a7f72
ArsamAryandoust <arsama@ethz.ch>
2023-04-08 21:06:45 +0200

    fixed path for test

--- src/test_dataset.py
+++ src/test_dataset.py
@@ -6,7 +6,7 @@ def test(config: dict, dataset_name: str):
   print('Testing {}'.format(dataset_name))
   
   # set the path to data we want to load
-  path_to_data = config['general']['path_to_data']
+  path_to_data = config['general']['path_to_data'] + dataset_name + '/'
   
   # load data
   train, val, test = load_data.load(dataset_name, path_to_data=path_to_data)
