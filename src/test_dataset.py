7260edae1cc27d94fa3d14c584aa6bb9e826ec07
ArsamAryandoust <arsama@ethz.ch>
2023-04-08 21:13:42 +0200

    added subtasks to tests

--- src/test_dataset.py
+++ src/test_dataset.py
@@ -1,14 +1,19 @@
 import selberai.data.load_data as load_data
 
-def test(config: dict, dataset_name: str):
+def test(config: dict, name: str):
   """
   """
-  print('Testing {}'.format(dataset_name))
+  print('Testing {}'.format(name))
   
-  # set the path to data we want to load
-  path_to_data = config['general']['path_to_data'] + dataset_name + '/'
+  # iterate over all subtasks
+  for subtask in config[name]['subtask_list']:
+    
+    # set the path to data we want to load
+    path_to_data = config['general']['path_to_data']+name+'/'+subtask+'/'    
+    
+    # load data
+    train, val, test = load_data.load(dataset_name, path_to_data=path_to_data)
   
-  # load data
-  train, val, test = load_data.load(dataset_name, path_to_data=path_to_data)
   
-  print("Successfully tested {}!".format(dataset_name))
+  
+  print("Successfully tested {}!".format(name))
