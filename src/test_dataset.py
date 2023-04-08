0a90389591daddb9df27c1b63be99056ebc82392
ArsamAryandoust <arsama@ethz.ch>
2023-04-08 20:49:29 +0200

    implemented load

--- src/test_dataset.py
+++ src/test_dataset.py
@@ -1,7 +1,14 @@
-
+import selberai.data.load_data as load_data
 
 def test(config: dict, dataset_name: str):
   """
   """
+  
+  # set the path to data we want to load
+  path_to_data = config['general']['path_to_data']
+  
+  # load data
+  load_data(dataset_name, path_to_data=path_to_data)
+  
   
   print("Successfully tested {}!".format(dataset_name))
