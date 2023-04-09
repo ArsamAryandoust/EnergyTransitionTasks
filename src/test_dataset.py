b9ae34ecf9be2f6e696275d17360cd9b9c2f0a33
ArsamAryandoust <arsama@ethz.ch>
2023-04-09 10:30:01 +0200

    implemented structure for test and analysis of datasets and subtasks

--- src/test_dataset.py
+++ src/test_dataset.py
@@ -3,20 +3,24 @@ import selberai.data.load_data as load_data
 def test(config: dict, name: str, subtask: str):
   """
   """
-  print('Testing {}'.format(name))
+  print('Testing {} for subtask {}'.format(name, subtask))
+  
+  ###
+  # Load data
+  ###
   
   # set the path to data we want to load
   path_to_data = config['general']['path_to_data']+name+'/'+subtask+'/'    
-  
-  # import api_key
-  with open(config['dataverse']['path_to_token'], 'r') as token:
-    api_key = token.read().replace('\n', '')
+  path_to_token = config['dataverse']['path_to_token']
   
   # load data
-  dataset_dict = load_data.load(name, path_to_data=path_to_data, 
-    token=api_key)
+  dataset = load_data.load(name, path_to_data=path_to_data, 
+    path_to_token=path_to_token)
+  
+  
+  ###
+  # Test variance of features and labels
+  ###
   
   
   
-  
-  print("Successfully tested {}!".format(name))
