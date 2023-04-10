0284a566b49131f1f014bf0b49ce8f3f9bea4065
ArsamAryandoust <arsama@ethz.ch>
2023-04-10 13:28:05 +0200

    new arrangement of subtask tests

--- src/test_dataset.py
+++ src/test_dataset.py
@@ -11,12 +11,11 @@ def test(config: dict, name: str, subtask: str):
   ###
   
   # set the path to data and token we want to load
-  path_to_data = config['general']['path_to_data']+name+'/'+subtask+'/'    
+  path_to_data = config['general']['path_to_data']   
   path_to_token = config['dataverse']['path_to_token']
   
   # load data
-  dataset = load_data.load(name, path_to_data=path_to_data, 
-    path_to_token=path_to_token)
+  dataset = load_data.load(name, subtask, path_to_data=path_to_data)
   
   
   ###
