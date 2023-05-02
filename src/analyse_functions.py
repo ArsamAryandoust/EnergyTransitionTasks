50b57393d608d0e8542d47deddf5dee506fbbabe
fdistefano99 <f.distefano.99@gmail.com>
2023-05-02 16:47:56 +0000

    Analyse functions updated

--- src/analyse_functions.py
+++ src/analyse_functions.py
@@ -1,5 +1,6 @@
 import math
 import torch
+import torch.distributions as distributions
 import seaborn as sns
 import matplotlib.pyplot as plt
 
@@ -12,10 +13,8 @@ def remove_outliers(x):
     x = x[~outlier_indices]
     return x
 
-def create_distribution(x, n_bins=10):
-    min, max = torch.min(x), torch.max(x)
-    x= (x - min) / (max - min)
-    histogram = torch.histc(x, bins=n_bins, min=0, max=1)
+def create_distribution(x, min, max, n_bins=10):
+    histogram = torch.histc(x, bins=n_bins, min=min, max=max)
     distribution = histogram / torch.sum(histogram)
     return distribution
     
@@ -24,23 +23,27 @@ def compute_js_divergence(p, q):
     part2 = torch.sum(q * torch.log2(2.0 * q + LOG_EPSILON) - q * torch.log2(q + p + LOG_EPSILON))
     return 0.5 * part1 + 0.5 * part2
 
-def plot_scores(scores, x_label, y_label):
+def plot_scores(scores, x_label, y_label, path, show=False):
     indices = [i for i in range(len(scores))]
     plt.bar(indices, scores)
     plt.xlabel(x_label)
     plt.ylabel(y_label)
     sns.set(style='darkgrid')
     plt.tight_layout()
-    plt.show()
+    plt.savefig(path)
+    if show:
+        plt.show()
 
-def plot_heatmap(x, x_label, y_label):
+def plot_heatmap(x, x_label, y_label, path, show=False):
     ax = sns.heatmap(x.numpy(), annot=True)
     ax.set(xlabel=x_label, ylabel=y_label)
     sns.set(style='darkgrid')
     plt.tight_layout()
-    plt.show()
+    plt.savefig(path)
+    if show:
+        plt.show()
 
-def simb_score(dataset, n_bins=10, features_to_skip=[], device='cpu'):
+def simb_score(dataset, n_bins=10000, features_to_skip=[], device='cpu'):
     n_features = dataset.shape[1]
     js_divergences = []
     uniform_distribution = torch.ones(n_bins).to(device) / n_bins
@@ -50,7 +53,8 @@ def simb_score(dataset, n_bins=10, features_to_skip=[], device='cpu'):
             continue
         dataset_slice = dataset[:, feature]
         dataset_slice = remove_outliers(dataset_slice)
-        distribution = create_distribution(dataset_slice, n_bins=n_bins)
+        min, max = torch.min(dataset_slice), torch.max(dataset_slice)
+        distribution = create_distribution(dataset_slice, min, max, n_bins=n_bins)
         js_divergence= compute_js_divergence(distribution, uniform_distribution)
         js_divergences.append(js_divergence)
     
@@ -58,7 +62,7 @@ def simb_score(dataset, n_bins=10, features_to_skip=[], device='cpu'):
     simb_score = torch.mean(js_divergences)
     return simb_score, js_divergences
 
-def stood_score(train_set, validation_set, test_set, n_bins=10, features_to_skip=[], device='cpu'):
+def stood_score(train_set, validation_set, test_set, n_bins=10000, features_to_skip=[], device='cpu'):
     n_features = train_set.shape[1]
     js_divergences_validation, js_divergences_test = [], []
 
@@ -73,9 +77,12 @@ def stood_score(train_set, validation_set, test_set, n_bins=10, features_to_skip
         validation_slice = remove_outliers(validation_slice)
         test_slice = remove_outliers(test_slice)
 
-        train_distribution = create_distribution(train_slice, n_bins=n_bins)
-        validation_distribution = create_distribution(validation_slice, n_bins=n_bins)
-        test_distribution = create_distribution(test_slice, n_bins=n_bins)
+        slice = torch.cat((train_slice, validation_slice, test_slice), dim=0)
+        min, max = torch.min(slice), torch.max(slice)
+
+        train_distribution = create_distribution(train_slice, min, max, n_bins=n_bins)
+        validation_distribution = create_distribution(validation_slice, min, max, n_bins=n_bins)
+        test_distribution = create_distribution(test_slice, min, max, n_bins=n_bins)
 
         js_divergence_validation = compute_js_divergence(train_distribution, validation_distribution)
         js_divergence_test = compute_js_divergence(train_distribution, test_distribution)
@@ -93,7 +100,7 @@ def stood_score(train_set, validation_set, test_set, n_bins=10, features_to_skip
 def ios_score(features, labels, features_to_skip=[], device='cpu'):
     n_features = features.shape[1]
     n_labels = labels.shape[1]
-    mean_deltas = torch.zeros(n_features - len(features_to_skip), n_labels)
+    mean_deltas = torch.zeros(n_features - len(features_to_skip), n_labels).to(device)
 
     for feature in range(n_features):
         slice = torch.hstack((features[:, feature].unsqueeze(1), labels))
