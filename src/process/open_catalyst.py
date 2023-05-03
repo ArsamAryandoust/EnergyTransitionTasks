2ba439e25a04d1482610a166553f1d1df3a3120a
ArsamAryandoust <arsama@ethz.ch>
2023-05-03 11:25:19 +0200

    implementing OC

--- src/process/open_catalyst.py
+++ src/process/open_catalyst.py
@@ -1,8 +1,9 @@
 import os
+import gc
+import json
 
-
-from config.load_oc import config_OC
 import ocpmodels
+from config.load_oc import config_OC
 from ocpmodels.datasets import TrajectoryLmdbDataset, SinglePointLmdbDataset
 
 
@@ -29,7 +30,7 @@ def process_all_datasets(config: dict, save: bool):
       
     elif subtask == 'oc22_is2res':
       process_oc22_is2res(config_opencat, save)
-      
+
 
 
 def load_dataset(path:str, single_point_lmdb:bool=False) -> (
@@ -50,9 +51,49 @@ def load_dataset(path:str, single_point_lmdb:bool=False) -> (
   
   
   
+def create_is2res_data(config_opencat: dict, dataset_list: (
+  list[ocpmodels.datasets.lmdb_dataset.SinglePointLmdbDataset] |
+  list[ocpmodels.datasets.lmdb_dataset.TrajectoryLmdbDataset]), save: bool):
+  """
+  """
+
+
+def create_s2ef_data(config_opencat: dict, path_list: list[str], save: bool):
+  """
+  """
+  
+  # create empty dictionary to save results
+  s2ef_data_dict = {}
+  id_counter = 0
+  
+  # iterate over passed path list
+  for path in path_list:
+  
+    # load dataset from currently iterated path
+    s2ef_dataset = load_dataset(path)
+    
+    # iterate over all datapoints
+    for datapoint in s2ef_dataset:
+      
+      # save datapoint information in results dictionary
+      s2ef_data_dict[id_counter] = {
+        'structure' : datapoint.pos,
+        'energy' : datapoint.y,
+        'forces' : datapoint.force
+      }
+  
+      
+  # free up memory
+  del s2ef_dataset
+  gc.collect()
+  
+  # save to json 
+  if save:
+    pass
+  
 def process_oc20_s2ef(config_opencat: dict, save: bool):
   """
-  """
+  """  
   
   # create all relevant data paths
   train_path = config_opencat['path_to_data_raw_oc20_s2ef_train']
@@ -65,17 +106,18 @@ def process_oc20_s2ef(config_opencat: dict, save: bool):
   test_ood_cat_path = config_opencat['path_to_data_raw_oc20_s2ef_test_ood_cat']
   test_ood_both_path = config_opencat['path_to_data_raw_oc20_s2ef_test_ood_both']
 
-
-  # load all datasets
-  train_dataset = load_dataset(train_path)
-  val_id_dataset = load_dataset(val_id_path)
-  val_ood_ads_dataset = load_dataset(val_ood_ads_path)
-  val_ood_cat_dataset = load_dataset(val_ood_cat_path)
-  val_ood_both_dataset = load_dataset(val_ood_both_path)
-  test_id_dataset = load_dataset(test_id_path)
-  test_ood_ads_dataset = load_dataset(test_ood_ads_path)
-  test_ood_cat_dataset = load_dataset(test_ood_cat_path)
-  test_ood_both_dataset = load_dataset(test_ood_both_path)
+  
+  # Create training data from all in distribution datasets
+  create_s2ef_data(config_opencat, [train_path, val_id_path, test_id_path], 
+    save)
+  
+  # create validation data from out-of-distribution datasets
+  create_s2ef_data(config_opencat, 
+    [val_ood_ads_path, val_ood_cat_path, val_ood_both_path], save)
+  
+  # create testing data from out-of-distribution datasets
+  create_s2ef_data(config_opencat, 
+    [test_ood_ads_path, test_ood_cat_path, test_ood_both_path], save)
   
   
   
@@ -127,6 +169,7 @@ def process_oc22_s2ef(config_opencat: dict, save: bool):
   val_ood_dataset = load_dataset(val_ood_path)
   test_id_dataset = load_dataset(test_id_path)
   test_ood_dataset = load_dataset(test_ood_path)
+  
   
   
   
