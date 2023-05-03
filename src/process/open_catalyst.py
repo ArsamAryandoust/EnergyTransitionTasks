8c34343b701a8f566c20b9f69b93f89a06b2ac75
ArsamAryandoust <arsama@ethz.ch>
2023-05-03 12:48:32 +0200

    implementing OC

--- src/process/open_catalyst.py
+++ src/process/open_catalyst.py
@@ -98,9 +98,7 @@ def create_s2ef_data(config_opencat: dict, path_list: list[str], save: bool):
   del s2ef_dataset
   gc.collect()
   
-  # save to json 
-  if save:
-    pass
+  return s2ef_data_dict
   
   
 def process_oc20_s2ef(config_opencat: dict, save: bool):
@@ -113,7 +111,6 @@ def process_oc20_s2ef(config_opencat: dict, save: bool):
   val_ood_ads_path = config_opencat['path_to_data_raw_oc20_s2ef_val_ood_ads']
   val_ood_cat_path = config_opencat['path_to_data_raw_oc20_s2ef_val_ood_cat']
   val_ood_both_path = config_opencat['path_to_data_raw_oc20_s2ef_val_ood_both']
-  
   #test_id_path = config_opencat['path_to_data_raw_oc20_s2ef_test_id']
   #test_ood_ads_path = config_opencat['path_to_data_raw_oc20_s2ef_test_ood_ads']
   #test_ood_cat_path = config_opencat['path_to_data_raw_oc20_s2ef_test_ood_cat']
@@ -121,10 +118,10 @@ def process_oc20_s2ef(config_opencat: dict, save: bool):
 
   
   # Create training data from all in distribution datasets
-  create_s2ef_data(config_opencat, [train_path, val_id_path], save)
+  s2ef_data_dict = create_s2ef_data(config_opencat, [train_path, val_id_path], save)
   
   # create testing data from out-of-distribution datasets
-  create_s2ef_data(config_opencat, 
+  s2ef_data_dict = create_s2ef_data(config_opencat, 
     [val_ood_ads_path, val_ood_cat_path, val_ood_both_path], save)
   
   
@@ -173,10 +170,11 @@ def process_oc22_s2ef(config_opencat: dict, save: bool):
 
 
   # Create training data from all in distribution datasets
-  create_s2ef_data(config_opencat, [train_path, val_id_path], save)
+  s2ef_data_dict = create_s2ef_data(config_opencat, [train_path, val_id_path], 
+    save)
   
   # create validation data from out-of-distribution datasets
-  create_s2ef_data(config_opencat, [val_ood_path], save)
+  s2ef_data_dict = create_s2ef_data(config_opencat, [val_ood_path], save)
   
 
   
