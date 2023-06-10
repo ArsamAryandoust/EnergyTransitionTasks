8f4dd7208f048163134e91a8f73071da3617307e
ArsamAryandoust <arsama@ethz.ch>
2023-06-10 12:30:57 +0200

    implemented spatio-temporal ood testing and validation splits for Polianna

--- src/config/load_pa.py
+++ src/config/load_pa.py
@@ -37,18 +37,27 @@ def config_PA(config: dict, subtask: str, save: bool) -> dict:
   config_polianna['path_to_data_subtask_test'] = (
     config_polianna['path_to_data_subtask'] + 'testing/')
   
+  
   # out of distribution test splitting rules in time
- 
+  year_list = [2004, 2006, 2008, 2009, 2012, 2013, 2014, 2018, 2019]
+  n_years = round(config_polianna['spatial_test_split'] * len(year_list))
+  random.seed = config['general']['seed']
+  test_years = random.sample(year_list, n_years)
   
   # out of distribution test splitting rules in space
- 
- 
-     
+  treaty_list = [0, 1, 2]
+  n_treaties = round(config_polianna['temporal_test_split'] * len(treaty_list))
+  random.seed = config['general']['seed']
+  test_treaties = random.sample(treaty_list, n_treaties)
+  
   # dictionary saving rules
   config_polianna['temporal_ood'] = {
+    'test_years' : test_years
   }
   config_polianna['spatial_ood'] = {
+    'test_treaties' : test_treaties
   }
+  
   
   # create directory structure for saving results
   if save:
@@ -69,7 +78,7 @@ def config_PA(config: dict, subtask: str, save: bool) -> dict:
       # create directory if not existent      
       check_create_dir(path)
   
-  # set subtask and return
+  # set subtask and seed, and return
   config_polianna['subtask'] = subtask
   config_polianna['seed'] = config['general']['seed']
   
